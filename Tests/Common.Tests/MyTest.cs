49dd41c2dd99c9a366e9e1acee2c042cf592cf29
zh.bq <423777014@qq.com>
2022-06-02 17:31:50 +0800

    mytest

---
+++ Tests/Common.Tests/MyTest.cs
@@ -0,0 +1,55 @@
+namespace Binance.Common.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using Moq.Protected;
+    using Xunit;
+    using Binance.Common;
+    public class MyTest
+    {
+        private string apiKey = "Sud7YtqxuBnwKDJZ7zgnGlZuOxssZ5QzrtvhkL7CfHMfP0fWglYzMScttIDsJ42v";
+        private string apiSecret = "QnU7QZwESqUnsuwYrs8KESVBXo4W8zgERMukgUhj9DR8phoY43WQZ0TjZgbbYbs9";
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("1 Started 1111111");
+
+            TestConnectivity_Response();
+
+            Console.WriteLine("2 End 2222222");
+        }
+
+
+        #region TestConnectivity
+        public static async void TestConnectivity_Response()
+        {
+            var responseContent = "{}";
+            var mockMessageHandler = new Mock<HttpMessageHandler>();
+            mockMessageHandler.Protected()
+                .SetupSendAsync("/api/v3/ping", HttpMethod.Get)
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(responseContent),
+                });
+            Market market = new Market(
+                new HttpClient(mockMessageHandler.Object),
+                apiKey: this.apiKey,
+                apiSecret: this.apiSecret);
+
+            var result = await market.TestConnectivity();
+
+            Assert.Equal(responseContent, result);
+        }
+        #endregion
+
+
+    }
+}
